303db58bc982e60b4e7b58c11199740c1d6613c8
claudia.stefan <claudeevine@yahoo.com>
2014-01-12 17:48:21 +0200

    First commit
    hope this works

---
+++ FOneManagementV2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FOneManagement.Startup))]
+namespace FOneManagement
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
